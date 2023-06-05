aa093cfd3db476d6d01d7c41318d85284ad2121c
Daniil <77277774+shamemask@users.noreply.github.com>
2023-06-05 22:38:11 +0300

    update

--- outletavto/settings.py
+++ outletavto/settings.py
@@ -128,7 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "images"),
 ]
 print(STATICFILES_DIRS)
-print(onlyfiles = [f for f in listdir(os.path.join(BASE_DIR, "static")) if isfile(join(os.path.join(BASE_DIR, "static"), f))])
+onlyfiles = [f for f in listdir(os.path.join(BASE_DIR, "static")) if isfile(join(os.path.join(BASE_DIR, "static"), f))]
+print(onlyfiles)
 # STATIC_ROOT = '/srv/www/outletavto/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
