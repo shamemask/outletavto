8556ac4e1de3eb170fdfd5635e00191d1ba1584f
Daniil <77277774+shamemask@users.noreply.github.com>
2023-06-05 07:44:14 +0300

    update

--- outletavto/settings.py
+++ outletavto/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "myapp/static"),
+    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "images"),
 ]
 # Default primary key field type
