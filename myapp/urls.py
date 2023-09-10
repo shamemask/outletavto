e2aac17c87a9d26ce12c3ee1e7de0ee1055bcf91
Daniil <77277774+shamemask@users.noreply.github.com>
2023-09-10 22:25:29 +0300

    Подкорректировал файлы

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,10 +4,11 @@ from django.conf.urls.static import static
 
 from authentication.views.email import send_confirmation_email, confirm_email
 from authentication.views.logout import logout
-from .views.main import index
+from .views.main import index, html
 
 urlpatterns = [
     path('', index, name='index'),
+    path('<str:html>', html, name='html'),
     # path('accounts/', include('allauth.urls')),
     path('logout', logout, name='logout'),
     path('confirm/', confirm_email, name='confirm_email'),
--- myapp/views/main.py
+++ myapp/views/main.py
@@ -3,7 +3,7 @@ import os
 from authentication.views.main import auth
 from myapp.main_logic.news_parser import get_news
 from django.views.decorators.csrf import csrf_exempt
-
+import os
 from django.shortcuts import render
 
 
@@ -15,11 +15,17 @@ def index(request):
     templ_dict = {}
     templ_dict['news'] = get_news("https://dvizhok.su/dvizhok-rss.rss")
     templ_dict['message'] = 'Запчасти в интернет-магазине'
+    path = os.getcwd() + '/static/outletauto/pages'
+    files = os.listdir(path)
+    templ_dict['files'] = [file.replace('.html','') for file in files]
     templ_dict.update(auth(request))
+
     request.session.save()
     return render(request, 'index.html', templ_dict)
 
-
+@csrf_exempt
+def html(request,html):
+    return render(request, f'pages\\{html}.html', {})
 
 
 
