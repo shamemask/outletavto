bada06d490d2a587165930d8db3319c866119ffb
Daniil <77277774+shamemask@users.noreply.github.com>
2023-09-18 20:25:11 +0300

    подвязал страницы

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,10 +4,45 @@ from django.conf.urls.static import static
 
 from authentication.views.email import send_confirmation_email, confirm_email
 from authentication.views.logout import logout
-from .views.main import index, html
+from .views.main import index, html, catalog_page, payment_page, news_page, universal_catalog_card_page, \
+    universal_catalog_tire_3_page, universal_catalog_tire_2_page, universal_catalog_tire_page, universal_catalog_page, \
+    testfilter_page, testcalendar_page, self_call_page, registration_page, recovery_page, promotion_page, profile_page, \
+    passenger_car_info_page, passenger_car_page, orders_page, news_page_page, modification_page, garage_page, \
+    favorite_page, company_page, club_page, catalog_personal_account_page, call_to_vin_page, basket_page, balance_page, \
+    authorization_page, add_auto_page
 
 urlpatterns = [
     path('', index, name='index'),
+    path('catalog', catalog_page, name='catalog_page'),
+    path('payment', payment_page, name='payment_page'),
+    path('news', news_page, name='news_page'),
+    path('add_auto', add_auto_page, name='add_auto_page'),
+    path('authorization', authorization_page, name='authorization_page'),
+    path('balance', balance_page, name='balance_page'),
+    path('basket', basket_page, name='basket_page'),
+    path('call_to_vin', call_to_vin_page, name='call_to_vin_page'),
+    path('catalog_personal_account', catalog_personal_account_page, name='catalog_personal_account_page'),
+    path('club', club_page, name='club_page'),
+    path('company', company_page, name='company_page'),
+    path('favorite', favorite_page, name='favorite_page'),
+    path('garage', garage_page, name='garage_page'),
+    path('modification', modification_page, name='modification_page'),
+    path('news_page', news_page_page, name='news_page_page'),
+    path('orders', orders_page, name='orders_page'),
+    path('passenger_car', passenger_car_page, name='passenger_car_page'),
+    path('passenger_car_info', passenger_car_info_page, name='passenger_car_info_page'),
+    path('profile', profile_page, name='profile_page'),
+    path('promotion', promotion_page, name='promotion_page'),
+    path('recovery', recovery_page, name='recovery_page'),
+    path('registration', registration_page, name='registration_page'),
+    path('self_call', self_call_page, name='self_call_page'),
+    path('testcalendar', testcalendar_page, name='testcalendar_page'),
+    path('testfilter', testfilter_page, name='testfilter_page'),
+    path('universal_catalog', universal_catalog_page, name='universal_catalog_page'),
+    path('universal_catalog_tire', universal_catalog_tire_page, name='universal_catalog_tire_page'),
+    path('universal_catalog_tire_2', universal_catalog_tire_2_page, name='universal_catalog_tire_2_page'),
+    path('universal_catalog_tire_3', universal_catalog_tire_3_page, name='universal_catalog_tire_3_page'),
+    path('universal_catalog_card', universal_catalog_card_page, name='universal_catalog_card_page'),
     path('<str:html>', html, name='html'),
     # path('accounts/', include('allauth.urls')),
     path('logout', logout, name='logout'),
--- myapp/views/main.py
+++ myapp/views/main.py
@@ -27,11 +27,225 @@ def index(request):
             count = f'{i // 10}1-{1 + (i // 10)}0'
             files_dict[count] = []
         files_dict[count].append(file)
-    templ_dict['files_dict'] = files_dict
+    templ_dict['files_dict2'] = files_dict
     templ_dict.update(auth(request))
 
     request.session.save()
     return render(request, 'index.html', templ_dict)
+
+@csrf_exempt
+def catalog_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Каталог'
+    templ_dict['page_class'] = 'catalog'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','catalog_page.html'), templ_dict)
+
+@csrf_exempt
+def payment_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Оплата заказа'
+    templ_dict['page_class'] = 'payment'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','payment_page.html'), templ_dict)
+
+@csrf_exempt
+def news_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Оплата заказа'
+    templ_dict['page_class'] = 'news'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','news_page.html'), templ_dict)
+
+
+@csrf_exempt
+def add_auto_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Баланс'
+    templ_dict['page_class'] = 'add-auto'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','add-auto_page.html'), templ_dict)
+@csrf_exempt
+def authorization_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Авторизация'
+    templ_dict['page_class'] = 'authorization'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','authorization_page.html'), templ_dict)
+@csrf_exempt
+def balance_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Баланс'
+    templ_dict['page_class'] = 'balance'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','balance_page.html'), templ_dict)
+@csrf_exempt
+def basket_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Корзина'
+    templ_dict['page_class'] = 'basket'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','basket_page.html'), templ_dict)
+@csrf_exempt
+def call_to_vin_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Vin'
+    templ_dict['page_class'] = 'call-to-vin'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','call-to-vin_page.html'), templ_dict)
+@csrf_exempt
+def catalog_personal_account_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'catalog-personal-account'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','catalog-personal-account_page.html'), templ_dict)
+@csrf_exempt
+def club_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'club'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','club_page.html'), templ_dict)
+@csrf_exempt
+def company_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'company'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','company_page.html'), templ_dict)
+@csrf_exempt
+def favorite_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'favorite'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','favorite_page.html'), templ_dict)
+@csrf_exempt
+def garage_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'garage'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','garage_page.html'), templ_dict)
+@csrf_exempt
+def modification_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'modification'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','modification_page.html'), templ_dict)
+@csrf_exempt
+def news_page_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'news_page'
+    return render(request, os.path.join('outletauto_page','news_page_page.html'), templ_dict)
+@csrf_exempt
+def orders_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'orders'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','orders_page.html'), templ_dict)
+@csrf_exempt
+def passenger_car_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'passenger-car'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','passenger-car_page.html'), templ_dict)
+@csrf_exempt
+def passenger_car_info_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'passenger-car-info'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','passenger-car-info_page.html'), templ_dict)
+@csrf_exempt
+def profile_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'profile'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','profile_page.html'), templ_dict)
+@csrf_exempt
+def promotion_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'promotion'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','promotion_page.html'), templ_dict)
+@csrf_exempt
+def recovery_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'recovery'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','recovery_page.html'), templ_dict)
+@csrf_exempt
+def registration_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'registration'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','registration_page.html'), templ_dict)
+@csrf_exempt
+def self_call_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'self-call'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','self-call_page.html'), templ_dict)
+@csrf_exempt
+def testcalendar_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = ''
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','TESTCALENDAR_page.html'), templ_dict)
+@csrf_exempt
+def testfilter_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = ''
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','TESTFILTER_page.html'), templ_dict)
+@csrf_exempt
+def universal_catalog_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'universal-catalog'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','universal-catalog_page.html'), templ_dict)
+@csrf_exempt
+def universal_catalog_tire_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'universal-catalog-tire'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','universal-catalog-tire_page.html'), templ_dict)
+@csrf_exempt
+def universal_catalog_tire_2_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'universal-catalog-tire-2'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','universal-catalog-tire-2_page.html'), templ_dict)
+@csrf_exempt
+def universal_catalog_tire_3_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'universal-catalog-tire-3'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','universal-catalog-tire-3_page.html'), templ_dict)
+@csrf_exempt
+def universal_catalog_card_page(request):
+    templ_dict = {}
+    templ_dict['page_title'] = 'Персональный каталог'
+    templ_dict['page_class'] = 'universal-catalog-card'
+    templ_dict.update(auth(request))
+    return render(request, os.path.join('outletauto_page','universal-catalog-card_page.html'), templ_dict)
 
 @csrf_exempt
 def html(request,html):
