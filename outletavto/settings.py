075722bfb2485aa43b6ce0560019ffc1a43cd8a0
Daniil <77277774+shamemask@users.noreply.github.com>
2023-09-28 21:04:29 +0300

    localhost

--- outletavto/settings.py
+++ outletavto/settings.py
@@ -31,7 +31,15 @@ SECRET_KEY = 'django-insecure-p)ophp_2lp@h27(jlomu&%v64lq=s%$_c6mv(&tzqeu=npim54
 
 
 
-ALLOWED_HOSTS = ['5-63-155-57.cloudvps.regruhosting.ru','[2a00:f940:2:4:2::4645]','xn--061-3edaa.xn--p1ai','outletavto-shamemask.b4a.run','127.0.0.1','5.63.155.57','outletavto.ru']
+ALLOWED_HOSTS = [
+    '5-63-155-57.cloudvps.regruhosting.ru',
+    '[2a00:f940:2:4:2::4645]',
+    'xn--061-3edaa.xn--p1ai',
+    'outletavto-shamemask.b4a.run',
+    '127.0.0.1','5.63.155.57',
+    'outletavto.ru',
+    'localhost'
+]
 
 
 # Application definition
