2aec5c302819e4c7fa821639ba4a38226b4de875
Daniil <77277774+shamemask@users.noreply.github.com>
2023-11-17 00:37:26 +0300

    сделать профиль

--- myapp/views/main.py
+++ myapp/views/main.py
@@ -1,5 +1,6 @@
 import os
 
+from authentication.UserModel import FizUser
 from authentication.views.main import auth
 from myapp.main_logic.news_parser import get_news,get_new
 from django.views.decorators.csrf import csrf_exempt
@@ -209,6 +210,12 @@ def profile_page(request):
     templ_dict['page_title'] = 'Профиль'
     templ_dict['page_class'] = 'profile'
     templ_dict.update(auth(request))
+    fields = templ_dict['user']._meta.fields
+    profile_dict = {}
+    for field in fields:
+        if field.name not in ['_state', 'id', 'last_login']:
+            profile_dict[field.verbose_name] = templ_dict['user'].__dict__[field.name]
+    templ_dict['profile_dict'] = profile_dict
     return render(request, os.path.join('outletauto_page','profile_page.html'), templ_dict)
 @csrf_exempt
 def promotion_page(request):
