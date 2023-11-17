b5dbbe116602ccfad283946deb2cce621218a512
Daniil <77277774+shamemask@users.noreply.github.com>
2023-11-17 17:58:30 +0300

    сделать профиль

--- myapp/views/main.py
+++ myapp/views/main.py
@@ -213,8 +213,10 @@ def profile_page(request):
     fields = templ_dict['user']._meta.fields
     profile_dict = {}
     for field in fields:
-        if field.name not in ['_state', 'id', 'last_login']:
-            profile_dict[field.verbose_name] = templ_dict['user'].__dict__[field.name]
+        if field.name not in ['_state', 'id', 'last_login','password', 'promo_code','terms_of_service','is_active']:
+            profile_dict[field.name] = [templ_dict['user'].__dict__[field.name],field.verbose_name]
+        if field.name == 'password':
+            profile_dict[field.verbose_name] = ['********',field.verbose_name]
     templ_dict['profile_dict'] = profile_dict
     return render(request, os.path.join('outletauto_page','profile_page.html'), templ_dict)
 @csrf_exempt
