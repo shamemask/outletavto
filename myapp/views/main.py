ad492860b1c1b7bf0070d20d529e1c8b1b2ec9d7
Daniil <77277774+shamemask@users.noreply.github.com>
2024-01-25 16:38:33 +0300

    Настроил отображение универсального каталога и пагинацию

--- myapp/views/main.py
+++ myapp/views/main.py
@@ -2,6 +2,7 @@ import os
 from functools import wraps
 
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from abcp_parser.models import CatalogItem
@@ -293,6 +294,30 @@ def universal_catalog_page(request):
     templ_dict = {}
     templ_dict['page_title'] = 'Персональный каталог'
     templ_dict['page_class'] = 'universal-catalog'
+    catalog = 'oils_catalog'
+    # получить все данные полей catalog из таблицы CatalogItem
+    catalog_items = CatalogItem.objects.filter(catalog=catalog)
+    # queryset to list, row to dict
+    catalog_items = list(catalog_items.values())
+    for i in range(len(catalog_items)):
+        other_info = catalog_items[i]['other_info']
+        catalog_items[i]['other_info'] = {}
+        for info_text in other_info.split('\n'):
+            if not info_text: continue
+            slt = info_text.split(':')
+            catalog_items[i]['other_info'][slt[0]] = slt[1]
+    page_count = 6
+    paginator = Paginator(catalog_items, page_count)
+    page = request.GET.get('page')
+    try:
+        objects = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом, установите на первую страницу
+        objects = paginator.page(1)
+    except EmptyPage:
+        # Если номер страницы больше максимального, установите на последнюю страницу
+        objects = paginator.page(paginator.num_pages)
+    templ_dict['catalog_items'] = objects
     templ_dict.update(auth(request))
     request.session.save()
     return render(request, os.path.join('outletauto_page','universal-catalog_page.html'), templ_dict)
